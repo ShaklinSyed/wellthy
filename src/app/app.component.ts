bdc6ed49ebdadb8e1a84ac39ac85f7cee54909a1
Shaklin Syed <shaklin.syed@gmail.com>
2022-07-23 16:42:06 +0530

    Installed material - mobile view completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { Platform } from '@angular/cdk/platform';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
+
   title = 'wellthy';
+
+  constructor(
+    public platform: Platform
+  ){
+
+  }
+
+  // Method to open app/play store links
+  goTo(url: string){
+    window.open(url)
+  }
 }
